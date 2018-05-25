4b0d3990ba19f81759ab2547c15f2de774205756
Tharuka Nimesha <nimesha1996@gmail.com>
2018-05-25 14:50:58 +0000

    login page done

---
+++ public/scripts/login.js
@@ -0,0 +1,12 @@
+var hour = new Date().getHours();
+
+$(document).ready(function(){
+  if(hour > 5 && hour <= 11)
+    $('body').css({"background-image":'url("https://i.imgur.com/odXkopW.png")', "background-size": "cover", "background-repeat": "no-repeat"});
+  else if(hour > 11 && hour <= 15)
+    $('body').css({"background-image":'url("https://i.imgur.com/HFEcueg.png")', "background-size": "cover", "background-repeat": "no-repeat"});
+  else if(hour > 15 && hour <= 18)
+    $('body').css({"background-image":'url("https://i.imgur.com/TsYjhS2.png")', "background-size": "cover", "background-repeat": "no-repeat"});
+  else if(hour > 18 || hour <= 5)
+    $('body').css({"background-image":'url("https://i.imgur.com/pQrc1hx.png")', "background-size": "cover", "background-repeat": "no-repeat"});
+});
