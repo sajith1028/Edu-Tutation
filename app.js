2266cab3748081af1fe3b9e7c045e384322b2351
Tharuka Nimesha <nimesha1996gmail.com>
2018-09-01 07:31:47 +0000

    new routes with all the home pages

--- app.js
+++ app.js
@@ -97,9 +97,9 @@ app.post("/register",function(req,res){
              from: 'studentenrolmentnsbm@gmail.com',
              to: 'nimesha1996@gmail.com',
              subject: 'Login Credentails',
+             html: '<center><div><p>Welcome to Akura Institute.</p><p>Please enter the password given below at the initial login</p> <p>Password : <strong>'+ randomPassword +'</strong></p> <br></br> <a href="https://akura-nimesha.c9users.io/login" style="background-color:#a0e5f8;border:1px solid #0f4b66;border-radius:18px;color:#2f353e;display:inline-block;font-family:sans-serif;font-size:13px;font-weight:bold;line-height:36px;text-align:center;text-decoration:none;width:200px;-webkit-text-size-adjust:none;mso-hide:all;">Click Here To Proceed</a><p>We wish you all the very best.<br></br>Akura Team.</p></div><center>'
            };
-             html:  '<center><div><p>Welcome to Akura Institute.</p><p>Please enter the password given below at the initial login</p> <p>Password : <strong>'+ randomPassword +'</strong></p> <br> <a href="https://akura-nimesha.c9users.io/login" style="background-color:#a0e5f8;border:1px solid #0f4b66;border-radius:18px;color:#2f353e;display:inline-block;font-family:sans-serif;font-size:13px;font-weight:bold;line-height:36px;text-align:center;text-decoration:none;width:200px;-webkit-text-size-adjust:none;mso-hide:all;">Click Here To Proceed</a><p>We wish you all the very best.<br>Akura Team.</div><center>'
-        
+
            transporter.sendMail(mailOptions, function(error, info){
              if (error) {
                console.log(error);
@@ -154,38 +154,108 @@ app.get("/login", function(req,res){
     res.render("login");
 });
 
-app.get("/studentProfile", function(req,res){
-    res.render("studentProfile");
+/*****************************************************
+ * Student Routes
+******************************************************/
+app.get("/student",function(req, res) {
+    res.render("student/studentHome");
 });
 
-app.get("/lecturerProfile", function(req,res){
-    res.render("studentProfile");
+app.get("/student/profile", function(req,res){
+    res.render("student/studentProfile");
 });
+
+app.get("/student/payments", function(req,res){
+    res.render("student/studentPayment");
+});
+
+app.get("/student/content", function(req,res){
+    res.render("student/studentContent");
+});
+
+app.get("/student/newsfeeds", function(req,res){
+    res.render("student/studentNewsfeed");
+});
+
+/******************************************************/
+
+
+
+/*****************************************************
+ * Lecturer Routes
+******************************************************/
+app.get("/lecturer",function(req, res) {
+    res.render("lecturer/lecturerHome");
+});
+
+app.get("/lecturer/profile", function(req,res){
+    res.render("lecturer/lecturerProfile");
+});
+
+app.get("/lecturer/income", function(req,res){
+    res.render("lecturer/lecturerIncome");
+});
+
+app.get("/lecturer/classes", function(req,res){
+    res.render("lecturer/lecturerClasses");
+});
+
+app.get("/lecturer/newsfeeds", function(req,res){
+    res.render("lecturer/lecturerNewsfeed");
+});
+
+/******************************************************/
+
+
+
+/*****************************************************
+ * Admin Routes
+******************************************************/
+app.get("/admin",function(req, res) {
+    res.render("admin/adminHome");
+});
+
+app.get("/admin/register", function(req,res){
+    res.render("admin/adminRegister");
+});
+
+app.get("/admin/payments", function(req,res){
+    res.render("admin/adminPayment");
+});
+
+app.get("/admin/attendance", function(req,res){
+    res.render("admin/adminAttendance");
+});
+
+app.get("/admin/newsfeeds", function(req,res){
+    res.render("admin/adminNewsfeed");
+});
+
+/******************************************************/
+
+
+
+/*****************************************************
+ * super admin Routes
+******************************************************/
+app.get("/superAdmin",function(req, res) {
+    res.render("superadmin/superAdminHome");
+});
+
+app.get("/superAdmin/income", function(req,res){
+    res.render("superadmin/superAdminIncome");
+});
+
+app.get("/superAdmin/payments", function(req,res){
+    res.render("superadmin/superAdminResults");
+});
+
+/******************************************************/
 
 app.get("/logout",function(req, res) {
    req.logout();
    req.flash("success","You logged out!");
    res.redirect("/landing");
-});
-
-app.get("/student",function(req, res) {
-    res.render("studentHome");
-});
-
-app.get("/lecturer",function(req, res) {
-    res.render("lecturerHome");
-});
-
-app.get("/admin",function(req, res) {
-    res.render("adminHome");
-});
-
-app.get("/superAdmin",function(req, res) {
-    res.render("superAdminHome");
-});
-
-app.get("/student/new",function(req, res) {
-    res.render("newstudent");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
